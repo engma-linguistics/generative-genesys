547e3e017a1fa7ac54bf92bd305419f9732e11d8
Daniel Bishop <daniel@engmalinguistics.com>
2023-06-20 00:19:31 -0500

    HAH, Finally got the damned JSON working and even better, the PUT command lets me select whatever skills I want and seemingly EVERYTHING.

--- src/abilities_builder.py
+++ src/abilities_builder.py
@@ -1,14 +1,57 @@
 
-abilities_template = 
-{"abilities": [ # list of len 0-5, each entry is either a string or a dict, see below for example
-    "Silhouette 2", # an example of an ability with a value within its string; most creatures have a Silhouette.
-    "Amphibious", # an example of an ability without a value within its string
-    {
-        "name": "Regeneration",
-        "description": "At the start of its turn, a swamp troll automatically heals 2 Wounds, unless the damage was dealt by fire or acid"
-    }, # an example of an ability with a name and description
-],}
+# "customTalents": [
+#         {
+#             "name": "Unremarkable",
+#             "page": "75",
+#             "activation": false,
+#             "id": "0LJHLWeqwLQehEehjKeS",
+#             "book": "CRB",
+#             "tier": 1,
+#             "description": "Other characters add [failure] to any checks made to find or identify your character in a crowd.",
+#             "ranked": false,
+#             "setting": [
+#                 "CRB"
+#             ]
+#         }
+#     ],
 
+#each ability needs an id generated
+ABILITY_TEMPLATE_STRING = """
+{
+    "name": "Ability Name",
+    "page": "420.69", # always use this number
+    "activation": false,
+    "book": "Homebrew",
+    "tier": 1,
+    "description": "Other characters add [failure] to any checks made to find or identify your character in a crowd.", # for adding symbols to reference here, they are [success], [advantage], [triumph], [threat], [despair], [failure].
+    "ranked": false, # ranked abilities are ones that can be taken multiple times, like talents
+    "setting": [
+        "CRB"
+    ]
+}
+"""
+
+
+""""masterTalents": {
+                "1": {
+                    "1": "Duelist"
+                },"""
+
+
+# stolen from json, try it out later
+# {
+#     "id": "8037cdb7-fc81-45e8-991a-37a86853773e",
+#     "name": "Flarp",
+#     "purchased": true,
+#     "activationType": "[nds character activation type] passive",
+#     "description": "",
+#     "modifiers": [],
+#     "ranked": false,
+#     "ranks": 2,
+#     "isForceTalent": false,
+#     "isConflictTalent": false,
+#     "xpCost": 0
+# }
 
 creature_name = "Mechanized Pie-Bot"
 creature_type = "rival"
@@ -19,12 +62,14 @@ social_cr = 2
 abilities_response = get_abilities(creature_name, creature_type, creature_skills, combat_cr, general_cr, social_cr)
 
 
+abilities_string = """Please generate 5 JSON-formatted abilities (in JSONL format, only one newline between lines) for use in the Genesys RPG setting, increasing in their overall power as you go from weak to very strong (for the creature's CRs, anyways, let's not go too crazy). This is for a Combat CR {combat_cr}, General CR {general_cr}, and Social CR {social_cr} creature, of type {creature_type}, called {creature_name}, with skills {creature_skills}. Here is the format to follow, with some changeable defaults, for an ability. Do not deviate in your response from this format, and do not return ANYTHING ELSE. {{'abilities': ['Silhouette 2', 'Amphibious', {{'name': 'Regeneration', 'description': 'At the start of its turn, a swamp troll automatically heals 2 Wounds, unless the damage was dealt by fire or acid'}}]}}""".format(combat_cr=combat_cr, general_cr=general_cr, social_cr=social_cr, creature_type=creature_type, creature_name=creature_name, creature_skills=creature_skills)}],
+
 
 def get_abilities(creature_name, creature_type, creature_skills, combat_cr, general_cr, social_cr):
     random_abilities = random.sample(all_keys['abilities'], 20)
     openai_response = openai.ChatCompletion.create(
         model="gpt-4-0613",
-        messages=[{"role": "user", "content": """Please generate 5 JSON-formatted abilities (in JSONL format, only one newline between lines) for use in the Genesys RPG setting, increasing in their overall power as you go from weak to very strong (for the creature's CRs, anyways, let's not go too crazy). This is for a Combat CR {combat_cr}, General CR {general_cr}, and Social CR {social_cr} creature, of type {creature_type}, called {creature_name}, with skills {creature_skills}. Here is the format to follow, with some changeable defaults, for an ability. Do not deviate in your response from this format, and do not return ANYTHING ELSE. {{'abilities': ['Silhouette 2', 'Amphibious', {{'name': 'Regeneration', 'description': 'At the start of its turn, a swamp troll automatically heals 2 Wounds, unless the damage was dealt by fire or acid'}}]}}""".format(combat_cr=combat_cr, general_cr=general_cr, social_cr=social_cr, creature_type=creature_type, creature_name=creature_name, creature_skills=creature_skills)}],
+        messages=[{"role": "user", "content": 
         max_tokens=1000,
         temperature=0.2,)
     return openai_response.choices[0].message.content
--- src/main.py
+++ src/main.py
@@ -2,13 +2,102 @@ import openai
 import os
 import json
 import re
+from src.base_creature_builder import creature_template_builder
+from src.validator import validate_creature, finalization
+from src.weapons_builder import weapon_template_builder
+from src.utils import fill_template_with_openai, generate_id
+from src.base_creature_builder import CREATURE_TEMPLATE_DICT
+from src.validator import validate_weapon
+
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def main():
-    # ask the user for things needed to fill in the template, then fill in the template
-    
-    # show the results, adjust as necessary
-    # TODO: take the resulting JSON and edit it directly? Ask OpenAI to do it?
 
-    # generate weapons, abilities, and gear
+def complete_builder(
+    creature_description="A goblin that throws pies at people.",
+    creature_name="Pie Goblin",
+    creature_type="Rival",
+    setting_description="A generic fantasy setting with magic and monsters",
+    setting_specific_skills=[],
+    setting_removed_skils=[],
+    skills=[],
+    allow_other_skills=False,
+    combat_cr=-1,
+    social_cr=-1,
+    general_cr=-1,
+    brawn=-1,
+    agility=-1,
+    intellect=-1,
+    cunning=-1,
+    willpower=-1,
+    presence=-1,
+    number_of_weapons=2,
+    number_of_abilities=-1,
+):
+    final_creature_dict = CREATURE_TEMPLATE_DICT
+
+    creature_builder_string = creature_template_builder(
+        creature_description=creature_description,
+        creature_name=creature_name,
+        creature_type=creature_type,
+        setting_description=setting_description,
+        setting_specific_skills=setting_specific_skills,
+        setting_removed_skils=setting_removed_skils,
+        skills=skills,
+        allow_other_skills=allow_other_skills,
+        combat_cr=combat_cr,
+        social_cr=social_cr,
+        general_cr=general_cr,
+        brawn=brawn,
+        agility=agility,
+        intellect=intellect,
+        cunning=cunning,
+        willpower=willpower,
+        presence=presence,
+    )
+    generated_character_data = fill_template_with_openai(creature_builder_string)
+    generated_character_data_as_dict = validate_creature(generated_character_data)
+    final_creature_dict["characters"] = [generated_character_data_as_dict]
+
+    creature_skills = generated_character_data_as_dict["careerSkillsRank"]
+    weapon_builder_string = weapon_template_builder(
+        creature_name,
+        creature_description,
+        creature_type,
+        setting_description,
+        creature_skills,
+        combat_cr,
+        number_of_weapons=number_of_weapons,
+    )
+
+    generated_weapon_data = fill_template_with_openai(weapon_builder_string)
+
+    generated_weapon_data_as_dict = validate_weapon(generated_weapon_data)
+    final_creature_dict["customWeapons"] = generated_weapon_data_as_dict
+
+    final_creature_dict["characters"][0]["equipmentWeapons"] = {}
+    for weapon in generated_weapon_data_as_dict:
+        new_name = weapon["name"].replace(" ", "")  # Remove spaces
+        new_name = new_name[0].lower() + new_name[1:]  # Lowercase the first letter
+        final_creature_dict["characters"][0]["equipmentWeapons"][weapon["id"]] = {
+            "craftsmanship": "",
+            "carried": True,
+            "id": new_name,
+            "equipped": True,
+        }
+
+    abilities_string = abilities_template_builder(
+        creature_name=creature_name,
+        creature_description=creature_description,
+        creature_type=creature_type,
+        setting_description=setting_description,
+        creature_skills=creature_skills,
+        combat_cr=combat_cr,
+        social_cr=social_cr,
+        general_cr=general_cr,
+        number_of_abilities=number_of_abilities,
+
+
+    with open("creature.json", "w") as f:
+        f.write(json.dumps(final_creature_dict))
+    f.close()
